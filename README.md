80a62302f67e789908c41707cabc3ec2362f42d4
Ravi Patel <ravi.patel1245@gmail.com>
2019-02-09 23:58:33 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # raviolliii.github.io
 Portfolio Site
+
+My personal website, **currently under construction**
---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+setTimeout(function() {
+    document.querySelector(".right").classList.toggle("hidden");
+}, 1000);
