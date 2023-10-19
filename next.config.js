08f6b698f2b1fca7209f02198cfdbe7ee9646a54
Ravi Patel <ravi.patel1245@gmail.com>
2023-10-19 15:48:39 -0400

    Change resource path from absolute to relative

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
  */
 const nextConfig = {
     output: 'export',
+    assetPrefix: './'
 
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
