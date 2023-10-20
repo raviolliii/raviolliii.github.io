f324e05e93cd75a3dc45bbee65f39129bc2f17dd
Ravi Patel <ravi.patel1245@gmail.com>
2023-10-20 01:40:43 -0400

    Updated build artifacts to latest

--- _next/static/ms8-viezP9jjIvBubNc4y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b021152f9c8e3cd3.css","static/chunks/pages/index-acf24fd6c3271196.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
