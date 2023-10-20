899e394804f7fec8f8e80ec484b6b5c6e9aa8978
Ravi Patel <ravi.patel1245@gmail.com>
2023-10-20 01:35:11 -0400

    Add copy button to commands

--- components/Info.js
+++ components/Info.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/info.module.css'
 
 export default function Info({ infoData }) {
     const { theme, title, subtitle, url } = infoData;
+    const copyText = "curl " + url;
 
     return (
         <div className={styles.container}>
@@ -10,7 +15,28 @@ export default function Info({ infoData }) {
             <div className={styles.code}>
                 <span className={styles['cmd-' + theme]}>curl</span>&nbsp;
                 {url}
+                <CopyButton text={copyText} />
             </div>
         </div>
     );
+}
+
+function CopyButton({ text }) {
+    const [isCopied, setIsCopied] = useState(false);
+
+    function copyCmd() {
+        navigator.clipboard.writeText(text);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 1000);
+    }
+
+    let containerClassNames = styles.copyBtn;
+    if (!isCopied) {
+        containerClassNames += " " + styles.hoverable;
+    }
+    return (
+        <div className={containerClassNames} onClick={!isCopied ? copyCmd : null}>
+            <FontAwesomeIcon icon={isCopied ? faCheck : faCopy} size='lg' />
+        </div>
+   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/global.css';
+
+// Configure Font Awesome to not import any custom CSS
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
