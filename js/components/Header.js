456ad1c539e8bcda5b544d2be399ed7590a09913
Ravi Patel <ravi.patel1245@gmail.com>
2023-10-25 13:17:47 -0400

    Update header subtitle for spoiler sections

--- js/components/Header.js
+++ js/components/Header.js
@@ -7,7 +7,10 @@ export default function Header() {
             <br />
             <p className={styles.subtitle}>
                 i'm Ravi, a software engineer. i just like getting coffee and building things -
-                hit some of the endpoints below to find out more about me (i promise they're safe haha)
+                hit some of the endpoints below to find out more about me 
+            </p>
+            <p className={styles.whisperTitle}>
+                (or hit the spoiler button in each section and i'll fetch the info for you)
             </p>
         </div>
     );
